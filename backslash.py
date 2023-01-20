b124d249c4ead10704befb4768801a318e0ae077
priyar1 <122872326+priyar1@users.noreply.github.com>
2023-01-20 10:57:13 +0530

    Add files via upload

---
+++ backslash.py
@@ -0,0 +1,9 @@
+# print('Hello Everyone Welcome to Python, \
+# class which is running in 2nd floor at \
+# josh innovations')
+
+# print("we are learning python programming from,\
+#  josh innovations in 2nd floor at 10:30AM")
+
+# print('Taumatawhakatangihangakoauauotamateatur\
+# ipukakapikimaungahoronukupokaiwhenuakiclstanatahu')
